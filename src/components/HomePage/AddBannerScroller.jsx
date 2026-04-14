4273c72360228e23bc9ba4c97cf730685aa51517
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-14 15:10:29 +0530

    ad banner component

---
+++ src/components/HomePage/AddBannerScroller.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+import iphoneImage from "../../assets/AdBanner/iphone.png"
+
+const banners = [
+  { image: iphoneImage },
+  { image: iphoneImage },
+  { image: iphoneImage },
+]
+
+export default function AddBannerScroller() {
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % banners.length)
+    }, 4000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="w-full bg-black overflow-hidden">
+
+      {/* Slider */}
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        {banners.map((banner, index) => (
+          <div
+            key={index}
+            className="min-w-full h-[220px] md:h-[380px] flex items-center justify-center"
+          >
+            <img
+              src={banner.image}
+              alt="banner"
+              className="h-full w-auto object-contain"
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Dots */}
+      <div className="flex justify-center gap-3 py-4">
+        {banners.map((_, i) => (
+          <button
+            key={i}
+            onClick={() => setCurrent(i)}
+            className={`w-3 h-3 rounded-full ${
+              i === current ? "bg-red-500" : "bg-gray-500"
+            }`}
+          />
+        ))}
+      </div>
+
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import TopBanner from '../components/TopBanner'
 import Header from '../components/Header'
+import AddBannerScroller from '../components/HomePage/AddBannerScroller'
 
 const Home = () => {
   return (
     <div>
         <TopBanner></TopBanner>
         <Header></Header>
+        <div className='px-5'>
+        <AddBannerScroller></AddBannerScroller>
+        </div>
         <p>
             home page
         </p>
