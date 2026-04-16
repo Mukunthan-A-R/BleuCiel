e8feb498c71eaa7b856bc72af7865bdb2745fa7e
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-16 17:31:51 +0530

    product search result page

--- src/pages/ProductSearch.jsx
+++ src/pages/ProductSearch.jsx
@@ -2,47 +2,9 @@ import { useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import ProductCard from "../components/ProductCard";
 import TopBanner from "../components/TopBanner";
+import { products } from "../data/products.js";
 
 const ProductSearch = ({ productsList = [] }) => {
-  const products = [
-    {
-      id: 1,
-      title: "The north coat",
-      price: 260,
-      oldPrice: 360,
-      rating: 5,
-      reviews: 65,
-      image: "https://via.placeholder.com/200",
-    },
-    {
-      id: 2,
-      title: "Gucci duffle bag",
-      price: 960,
-      oldPrice: 1160,
-      rating: 5,
-      reviews: 65,
-      image: "https://via.placeholder.com/200",
-    },
-    {
-      id: 3,
-      title: "RGB liquid CPU Cooler",
-      price: 160,
-      oldPrice: 170,
-      rating: 4,
-      reviews: 65,
-      image: "https://via.placeholder.com/200",
-    },
-    {
-      id: 4,
-      title: "Small BookSelf",
-      price: 360,
-      oldPrice: null,
-      rating: 5,
-      reviews: 65,
-      image: "https://via.placeholder.com/200",
-    },
-  ];
-
   const location = useLocation();
 
   const params = new URLSearchParams(location.search);
